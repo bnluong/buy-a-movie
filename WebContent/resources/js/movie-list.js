b3daf8cd07c67b6f793bf520574f47817a9ad68b
Bao Luong <bnluong@uci.edu>
2020-04-20 17:02:46 -0700

    Configured a DataSource
    Implemented the Movie List that displays top 20 rated movies

---
+++ WebContent/resources/js/movie-list.js
@@ -0,0 +1,62 @@
+/**
+ * This example is following frontend and backend separation.
+ *
+ * Before this .js is loaded, the html skeleton is created.
+ *
+ * This .js performs two steps:
+ *      1. Use jQuery to talk to backend API to get the json data.
+ *      2. Populate the data to correct html elements.
+ */
+
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data
+ * into html elements
+ * 
+ * @param resultData
+ *            jsonObject
+ */
+function handleMovieListResult(resultData) {
+    console.log("handleMovieListResult: populating movie list table from resultData");
+
+    // Populate the movie list table
+    // Find the empty table body by id "movie_list_table_body"
+    let movieListTableBodyElement = jQuery("#movie_list_table_body");
+
+    // Iterate through resultData, no more than 10 entries
+    for(let i = 0; i < Math.min(20, resultData.length); i++) {
+        // Concatenate the html tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        
+        /*
+		TODO: Implement Single Movie page (opened by clicking on a single movie on the list)
+        rowHTML +=
+            "<th>" +
+            // Add a link to movie-star.html with id passed with GET url parameter
+            // display movie's title for the link text
+            '<a href="single-movie.html?id=' + resultData[i]['movie_id'] + '">' + resultData[i]["movie_title"] + '</a>' + 
+            "</th>";
+        */
+        
+        rowHTML += "<th>" + resultData[i]["movie_title"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+        
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        movieListTableBodyElement.append(rowHTML);
+    }
+}
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser
+ */
+// Makes the HTTP GET request and registers on success callback function
+// handleMovieListResult
+jQuery.ajax({
+    dataType: "json", 		// Setting return data type
+    method: "GET", 			// Setting request method
+    url: "api/movie-list", 	// Setting request url, which is mapped by StarsServlet in Stars.java
+    // Setting callback function to handle data returned successfully by the MovieListServlet
+    success: (resultData) => handleMovieListResult(resultData)
+});
