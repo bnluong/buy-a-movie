efc0dab2dfb9d148ddf40fc3c098800cbd684e8f
Bao Luong <bnluong@uci.edu>
2020-05-05 22:35:37 -0700

    Implemented 'Browse by Genres' and 'Browse by Titles' Sections
    	- Added links to the title in 'Random Movies' section
    	- Tuned up some formatting
    	- TODO: Login

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -7,8 +7,9 @@
  * @param resultData jsonObject
  */
 function handleResult(resultData) {
-    console.log("handleResult: handling resultData");
-    handleRandomMovies(resultData)
+    handleRandomMovies(resultData['randomMovies']);
+    handleListOfGenres(resultData['listOfGenres']);
+    handleBrowseByTitles();
 }
 
 /**
@@ -16,34 +17,84 @@ function handleResult(resultData) {
  * @param randomMovies jsonObject
  */
 function handleRandomMovies(randomMovies) {
-    console.log("handleRandomMovies: populating random movies");
     for (let i = 0; i < randomMovies.length; i++) {
         let movieImgID = '#movie' + (i + 1) + 'Img';
-        let movieTitleID = '#movie' + (i + 1) + 'Title';
-        let titleHTML = randomMovies[i]['title'] + ' (' + randomMovies[i]['year'] + ')';
-
         var imdbURL = 'https://www.imdb.com/title/' + randomMovies[i]['id'];
         $.ajax({
-        	url: 'http://www.whateverorigin.org/get?url=' + encodeURIComponent(imdbURL) + '&callback=?',
-        	dataType: 'json',
-        	async: false,
-        	success: function(data) {
-        		var tempDOM = document.createElement('div');
-        		tempDOM.innerHTML = data.contents;
+            url: 'http://www.whateverorigin.org/get?url=' + encodeURIComponent(imdbURL) + '&callback=?',
+            dataType: 'json',
+            async: false,
+            success: function(data) {
+                var tempDOM = document.createElement('div');
+                tempDOM.innerHTML = data.contents;
 
-        		var poster = tempDOM.getElementsByClassName('poster');
-        		if (poster[0] != null) {
-        			let tempDOM = document.createElement('div');
-        			tempDOM.innerHTML = poster[0].innerHTML;
-        			let posterURL = tempDOM.querySelector('img').getAttribute('src');
-        			$(movieImgID).attr("src", posterURL);
-        		} else {
-        			$(movieImgID).attr("src", 'img/empty-box-art.png');
-        		}
-        	}
+                var poster = tempDOM.getElementsByClassName('poster');
+                if (poster[0] != null) {
+                    let tempDOM = document.createElement('div');
+                    tempDOM.innerHTML = poster[0].innerHTML;
+                    let posterURL = tempDOM.querySelector('img').getAttribute('src');
+                    $(movieImgID).attr("src", posterURL);
+                } else {
+                    $(movieImgID).attr("src", 'img/empty-box-art.png');
+                }
+            }
         });
-        
-        $(movieTitleID).html('<p class="h6">' + titleHTML);
+
+        //  Handle random movie title
+        //<a class="h6" href="">asdsad</a>
+        let titleID = '#movie' + (i + 1) + 'Title';
+        let titleURLID = '.title-' + i;
+        let titleHTML = '<a class="h6 title-'+ i + '" href=""></a>';
+        let titleURLHTML = 'movie-info.html?id=' + randomMovies[i]['id'];
+        $(titleID).append(titleHTML);
+        $(titleURLID).attr('href', titleURLHTML)
+        $(titleURLID).text(randomMovies[i]['title'] + ' (' + randomMovies[i]['year'] + ')');
+    }
+}
+
+/**
+ * Handles Browse by Genres section
+ * @param listOfGenres jsonArray
+ */
+function handleListOfGenres(listOfGenres) {
+    // HTML: <a class="btn btn-outline-dark btn-sm mx-1 my-1" href="login.html" role="button" style="border-radius:10px; font-size: 15px;">Action</a>
+    for (let i = 0; i < listOfGenres.length; i++) {
+        let btnHTML = '<a class="btn btn-outline-dark btn-sm mx-1 my-1 genreBtn-' + i + '" href="#" role="button" style="border-radius:15px; font-size: 18px;"></a>\n';
+        let hrefHTML = '#';
+        let btnID = '.genreBtn-' + i;
+        $('#browseByGenres').append(btnHTML);
+        $(btnID).attr('href', hrefHTML);
+        $(btnID).text(listOfGenres[i]['genre_name']);
+    }
+}
+
+/**
+ * Handles Browse by Titles section
+ * @param None
+ */
+function handleBrowseByTitles() {
+    // Handle browse by numeric (#browseByTitleNumeric)
+    // <a href="#" class="badge badge-dark badge-pill" style="font-size: 15px;">1</a>
+    const numArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    for (let i = 0; i < numArray.length; i++) {
+        let bdgHTML = '<a href="#" class="badge badge-dark badge-pill mx-1 my-1 numBdg-' + i + '" style="font-size: 18px;"></a>\n';
+        let hrefHTML = '#';
+        let bdgID = '.numBdg-' + i;
+        $('#browseByTitleNumeric').append(bdgHTML);
+        $(bdgID).attr('href', hrefHTML);
+        $(bdgID).text(numArray[i]);
+    }
+
+    // Handle browse by alphabet (#browseByTitleAlpha)
+    // <a href="#" class="badge badge-dark badge-pill" style="font-size: 15px;">B</a>
+    const alphaArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    for (let i = 0; i < alphaArray.length; i++) {
+        let bdgHTML = '<a href="#" class="badge badge-dark badge-pill mx-1 my-1 alpBdg-' + i + '" style="font-size: 18px;"></a>\n';
+        let hrefHTML = '#';
+        let bdgID = '.alpBdg-' + i;
+        $('#browseByTitleAlpha').append(bdgHTML);
+        $(bdgID).attr('href', hrefHTML);
+        $(bdgID).text(alphaArray[i]);
     }
 }
 
@@ -51,8 +102,7 @@ function handleRandomMovies(randomMovies) {
  * Once this .js is loaded, following scripts will be executed by the browser
  */
 // Makes the HTTP GET request and registers on success callback function handleResult
-
-$.ajax("api/index", {
-    method: "GET",
+$.ajax('api/index', {
+    method: 'GET',
     success: handleResult
 });
--- src/IndexServlet.java
+++ src/IndexServlet.java
@@ -19,75 +19,57 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
  * Servlet implementation class IndexServlet
  */
 @WebServlet(name = "IndexServlet", urlPatterns = "/api/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 5L;
+	
 	// Create a dataSource which registered in web.xml
 	@Resource(name = "jdbc/moviedb")
 	private DataSource dataSource;
-	
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("application/json"); // Response mime type
 
 		// Output stream to STDOUT
 		PrintWriter out = response.getWriter();
+		
 		try {
 			// Get a connection from dataSource
 			Connection dbConnection = dataSource.getConnection();
-
-			// Declare our statement
-			Statement statement = dbConnection.createStatement();
-
-			// Declare the query
-			String query = 
-					"SELECT m1.id, title, year\n" + 
-					"FROM movies AS m1 \n" + 
-					"JOIN (SELECT id FROM movies ORDER BY RAND() LIMIT 10) as m2 ON m1.id = m2.id \n" + 
-					"LIMIT 3";
-
-			// Perform the query
-			ResultSet resultSet = statement.executeQuery(query);
-
-			// Declare a jsonArray to store the result of the query
-			JsonArray jsonArray = new JsonArray();
-
-			// Iterate through each row of resultSet
-			while(resultSet.next()) {
-				// Create a JsonObject based on the data we retrieve from the result set
-				JsonObject jsonObject = new JsonObject();
-				jsonObject.addProperty("id", resultSet.getString("id"));
-				jsonObject.addProperty("title", resultSet.getString("title"));
-				jsonObject.addProperty("year", resultSet.getString("year"));
-				jsonArray.add(jsonObject);
-			}
-
-			// write JSON string to output
-			out.write(jsonArray.toString());
-			// set response status to 200 (OK)
+			
+			JsonObject resultData = new JsonObject();
+			
+			JsonArray listOfGenres = getListOfGenres(dbConnection);
+			JsonArray randomMovies = getRandomMovies(dbConnection, 3);
+			
+			resultData.add("randomMovies", randomMovies);
+			resultData.add("listOfGenres", listOfGenres);
+			
+			// Write JSON string to output
+			out.write(resultData.toString());
+			// Set response status to 200 (OK)
 			response.setStatus(200);
-
-			resultSet.close();
-			statement.close();
+			// Close DB Connection
 			dbConnection.close();
 		} catch (Exception e) {
-			// write error message JSON object to output
+			// Write error message JSON object to output
 			JsonObject jsonObject = new JsonObject();
 			jsonObject.addProperty("errorMessage", e.getMessage());
 			out.write(jsonObject.toString());
 
-			// set reponse status to 500 (Internal Server Error)
+			// Set reponse status to 500 (Internal Server Error)
 			response.setStatus(500);
 		}
 		out.close();
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -95,5 +77,66 @@ public class IndexServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	private JsonArray getRandomMovies(Connection dbConnection, Integer numMovies) throws SQLException {
+		String query = 
+				"SELECT m1.id, title, year\n" + 
+				"FROM movies AS m1 \n" + 
+				"JOIN (SELECT id FROM movies ORDER BY RAND() LIMIT 10) as m2 ON m1.id = m2.id \n" + 
+				"LIMIT ?";
 
+		// Prepare the statement
+		PreparedStatement statement = dbConnection.prepareStatement(query);
+		statement.setInt(1, numMovies); // 1 represents the 1st "?" from the query
+
+		// Execute the query
+		ResultSet rSet = statement.executeQuery();
+
+		// Create an JsonArray object to hold the data
+		JsonArray jArray = new JsonArray();
+		
+		// Add the retrieved data to the array
+		while(rSet.next()) {			
+			// Create a JsonObject based on the data we retrieve from the result set
+			JsonObject jObject = new JsonObject();
+			jObject.addProperty("id", rSet.getString("id"));
+			jObject.addProperty("title", rSet.getString("title"));
+			jObject.addProperty("year", rSet.getString("year"));
+			
+			jArray.add(jObject);
+		}
+
+		rSet.close();
+		statement.close();
+		return jArray;
+	}
+
+	private JsonArray getListOfGenres(Connection dbConnection) throws SQLException {
+		String query = 
+				"SELECT id as genre_id, name as genre_name \n" + 
+				"FROM genres \n" + 
+				"ORDER by genre_name ASC";
+
+		// Prepare the statement
+		PreparedStatement statement = dbConnection.prepareStatement(query);
+
+		// Execute the query
+		ResultSet rSet = statement.executeQuery();
+
+		// Create an JsonArray object to hold the data
+		JsonArray jArray = new JsonArray();
+		
+		// Add the retrieved data to the array
+		while(rSet.next()) {
+			JsonObject jObject = new JsonObject();
+			jObject.addProperty("genre_id", rSet.getString("genre_id"));
+			jObject.addProperty("genre_name", rSet.getString("genre_name"));
+
+			jArray.add(jObject);
+		}
+
+		rSet.close();
+		statement.close();
+		return jArray;
+	}
 }
